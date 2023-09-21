aae9230938fc35511e48ceabbc2a2d75f5f1095b
Ayrton H. Torres <ayrton.torres.1995@gmail.com>
2023-09-21 17:03:32 -0300

    Initial commit

---
+++ primerproyecto/src/primerproyecto/Ejercicio1.java
@@ -0,0 +1,27 @@
+package primerproyecto;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo");
+		
+		char unaLetra = 'a';
+		System.out.println(unaLetra);
+		
+		boolean booleano = true;
+		System.out.println(booleano);
+		
+		int unEntero = 99;
+		System.out.println(unEntero);
+		
+		double numDouble = 1.83;
+		System.out.println(numDouble);
+		
+		float numFlotante = 1.49f;
+		System.out.println(numFlotante);
+		
+		
+		
+	}
+}
