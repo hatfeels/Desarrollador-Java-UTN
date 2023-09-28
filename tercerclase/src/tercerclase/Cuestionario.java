1592e3b56228551cca7093185b690a8127110125
Ayrton H. Torres <ayrton.torres.1995@gmail.com>
2023-09-28 14:39:16 -0300

    comienzo de la 4ta clase

---
+++ tercerclase/src/tercerclase/Cuestionario.java
@@ -0,0 +1,12 @@
+package tercerclase;
+
+public class Cuestionario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hola que tal?".toUpperCase().replace(
+				"hola","chau"));
+	}
+
+}
